09ae1f214e450d46ec3ac40c699203b3ded0337d
Axel DEFO <axeldefo22@gmail.com>
2025-01-20 16:15:52 +0100

    Etape 12. Manipuler un keystore JAVA pour accéder au certificat

--- src/MySignaturePadesPdf.java
+++ src/MySignaturePadesPdf.java
@@ -3,29 +3,79 @@ import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.security.Security;
+import java.util.Enumeration;
+import java.util.Scanner;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class MySignaturePadesPdf {
+
+    // Attributs pour stocker la clé privée, le certificat et la chaîne de certificats
+    private PrivateKey privateKey;
+    private X509Certificate certificate;
+    private Certificate[] certificateChain;
 
     // Nom du fichier PDF source
     public static final String SRC_PDF = "monPdf.pdf";
 
+    // Constructeur pour charger le Keystore
+    public MySignaturePadesPdf(String pP12File, String pMotDePasse) {
+        // Ajout du fournisseur BouncyCastle pour la gestion de cryptographie
+        Security.addProvider(new BouncyCastleProvider());
+        try {
+            // Chargement du fichier PKCS12 avec BouncyCastle
+            KeyStore keystore = KeyStore.getInstance("pkcs12", "BC");
+            keystore.load(new FileInputStream(pP12File), pMotDePasse.toCharArray());
+
+            // Boucle pour obtenir les alias et vérifier si c'est une clé privée
+            Enumeration<String> aliases = keystore.aliases();
+            while (aliases.hasMoreElements()) {
+                String alias = aliases.nextElement();
+
+                // Vérification de la clé privée
+                if (keystore.isKeyEntry(alias)) {
+                    // Récupération de la clé privée
+                    privateKey = (PrivateKey) keystore.getKey(alias, pMotDePasse.toCharArray());
+                    // Récupération du certificat
+                    certificate = (X509Certificate) keystore.getCertificate(alias);
+                    // Récupération de la chaîne de certificats
+                    certificateChain = keystore.getCertificateChain(alias);
+                    break; // On arrête dès qu'on a trouvé la clé et le certificat
+                }
+            }
+
+            if (certificate != null) {
+                System.out.println("Certificat trouvé : " + certificate.getSubjectDN());
+            } else {
+                System.out.println("Aucun certificat trouvé.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Méthode pour afficher le certificat
+    public X509Certificate getCertificate() {
+        return certificate;
+    }
+
     // Méthode pour générer le PDF
     public void generatePdf() {
         try {
-            // Création d'un PdfWriter
             PdfWriter writer = new PdfWriter(new FileOutputStream(SRC_PDF));
-
-            // Création d'un PdfDocument à partir du PdfWriter
             PdfDocument pdf = new PdfDocument(writer);
-
-            // Création d'un document à partir du PdfDocument
             Document document = new Document(pdf);
 
-            // Ajout d'un paragraphe au document
             document.add(new Paragraph("Je vais signer un fichier PDF"));
-
-            // Fermeture du document
             document.close();
 
             System.out.println("Le fichier PDF a été généré avec succès !");
@@ -35,8 +85,24 @@ public class MySignaturePadesPdf {
     }
 
     public static void main(String[] args) {
-        MySignaturePadesPdf pdfGenerator = new MySignaturePadesPdf();
+        // Demande du mot de passe à l'utilisateur
+        String p12File = "C:\\Users\\axeld\\OneDrive\\Documents\\cyber\\TP_Signature\\src\\AxelDefo_cert_sign.p12";
+
+        Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Entrez le mot de passe du fichier .p12 : ");
+        String pMotDePasse = scanner.nextLine();
+
+        // Création de l'objet et chargement du keystore
+        MySignaturePadesPdf pdfGenerator = new MySignaturePadesPdf(p12File, pMotDePasse);
+
+        // Affichage des informations du certificat
+        X509Certificate cert = pdfGenerator.getCertificate();
+        if (cert != null) {
+            System.out.println("Certificat : " + cert.getSubjectDN());
+        }
+
+        // Appel de la méthode pour générer le PDF
         pdfGenerator.generatePdf();
-        System.out.println("Fin de la génération du PDF");
     }
 }
